601af8bf9ff8029e2145a01ac9a5adea2308e320
Dongathan-Jong <duclingsyt@gmail.com>
2024-07-06 22:06:48 -0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -26,6 +26,39 @@ void setup()
 
 void loop()
 {
+  if (digitalRead(BUTTON) == 1 && digitalRead(LEFTRED) == 1 && digitalRead(RIGHTGREEN) == 1)
+  {
+    digitalWrite(RIGHTGREEN,LOW);
+    digitalWrite(RIGHTYELLOW,HIGH);
+    delay(400);
+  }
+  
+  if (digitalRead(BUTTON) == 1 && digitalRead(RIGHTYELLOW) == 1 && digitalRead(LEFTRED) == 1)
+  {
+   	digitalWrite(RIGHTYELLOW,LOW);
+    digitalWrite(RIGHTRED,HIGH);
+    digitalWrite(LEFTRED,LOW);
+    digitalWrite(LEFTGREEN,HIGH); 
+    delay(400);
+  }
 
+  if (digitalRead(BUTTON) == 1 && digitalRead(LEFTYELLOW) == 1 && digitalRead(RIGHTRED) == 1)
+  {
+    digitalWrite(LEFTYELLOW,LOW);
+    digitalWrite(LEFTRED,HIGH);
+    digitalWrite(RIGHTRED,LOW);
+    digitalWrite(RIGHTGREEN,HIGH); 
+    delay(400);
+  }
+ 
+  if (digitalRead(BUTTON) == 1 && digitalRead(RIGHTRED) == 1 && digitalRead(LEFTGREEN) == 1)
+  {
+    digitalWrite(LEFTGREEN,LOW);
+    digitalWrite(LEFTYELLOW,HIGH);
+    delay(400);
+  }
+ 
+ 
+  
  
 }
